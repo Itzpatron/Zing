bcef0ce1d2952753588afcfaefbc16ee0a9ccbda
PATRON <137694181+Itzpatron@users.noreply.github.com>
2024-12-26 16:47:10 +0100

    Update qr.js

--- qr.js
+++ qr.js
@@ -58,17 +58,17 @@ router.get('/', async (req, res) => {
 				   let b64data = Buffer.from(data).toString('base64');
 				   let session = await Qr_Code_By_STAR_KING.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { document: data, fileName: "creds.json" })
 				   let SIGMA_MD_TEXT = `
-          _*PAIR Successful 🌟*_
+          _*PAIR Successful *_
 put the above ID in the sessID variable when deploying .
-Use this Session ID for all bots by STAR KING.
+Use this Session ID for all bots by PATRON.
 ╔═════◇
 ║       『••• STAR KING•••』
-║ *Channel:* _https://whatsapp.com/channel/0029VamU5H1DuMRYiHQ9vI09_
-║ *Main GC:* _https://chat.whatsapp.com/DC38hUUVVaa1vp573QBYBF_
-║ *Github:* _https://github.com/STAR-KING0_
-║ *Owner:* _https://wa.me/2348100835767_
+║ *Channel:* _https://whatsapp.com/channel/0029Val0s0rIt5rsIDPCoD2q_
+║ *Main GC:* _https://chat.whatsapp.com/LFtURSKepenEp9ENPk9uJ1_
+║ *Github:* _https://github.com/Itzpatron_
+║ *Owner:* _https://wa.me/8133729715
 ║ *Note :*_Do not provide your SESSION_ID to_
-║ _anyone otherwise that can access your WA messages_
+║ _anyone otherwise they can access your WA messages_
 ║ _*Follow Me and Star my repo for more 🫡.*_
 ╚════════════════════════╝`
 	 await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
